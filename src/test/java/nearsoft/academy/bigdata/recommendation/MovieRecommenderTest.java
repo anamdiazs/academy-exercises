7741ac9cc97488b0b4702e57c771f9d6504fa22d
Ana María Díaz <anamdiazs1212@gmail.com>
2022-04-18 22:40:14 -0500

    Change the route of movies.txt

--- src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommenderTest.java
+++ src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommenderTest.java
@@ -9,18 +9,17 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.hasItem;
-
 public class MovieRecommenderTest {
     @Test
     public void testDataInfo() throws IOException, TasteException {
         //download movies.txt.gz from 
         //    http://snap.stanford.edu/data/web-Movies.html
-        MovieRecommender recommender = new MovieRecommender("/path/to/movies.txt.gz");
+        MovieRecommender recommender = new MovieRecommender("./movies.txt.gz");
         assertEquals(7911684, recommender.getTotalReviews());
         assertEquals(253059, recommender.getTotalProducts());
         assertEquals(889176, recommender.getTotalUsers());
 
-        List<String> recommendations = recommender.getRecommendationsForUser("A141HP4LYPWMSR");
+        List<String> recommendations = recommender.getRecommendations("A141HP4LYPWMSR");
         assertThat(recommendations, hasItem("B0002O7Y8U"));
         assertThat(recommendations, hasItem("B00004CQTF"));
         assertThat(recommendations, hasItem("B000063W82"));
@@ -28,3 +27,5 @@ public class MovieRecommenderTest {
     }
 
 }
+
+
